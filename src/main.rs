2cc7de31db45617ba17e461b47c684bbca730f3a
whiteuuii <whiteuuii@gmail.com>
2026-03-11 17:08:26 +0500

    Stable version

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-mod db;
-
-#[allow(d)]
-
 use axum::extract::State;
 use axum::http::{StatusCode, Uri};
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Json, Router};
-use chrono::Utc;
+use chrono::{NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json;
 use serde_json::json;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::{FromRow, Pool, Postgres};
+use sqlx::{FromRow, Pool, Postgres, Row};
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let db = Arc::new(
-        AppState::new("postgres://postgres:l1nux=nosex@localhost/blogdata")
+        AppState::new("postgres://postgres:l1nux=nosex@localhost/BlogData")
             .await
             .expect("Failed to connect to the database"),
     );
@@ -97,7 +93,7 @@ async fn update_post(
     let id = id_from_uri(uri).expect("Invalid request ID");
 
     match db.update_post(id, updated_post).await {
-        Ok(post) => (StatusCode::OK, Json(json!(post))).into_response(),
+        Ok(_) => StatusCode::OK.into_response(),
         Err(e) => (
             StatusCode::INTERNAL_SERVER_ERROR,
             Json(json!({"error": e.to_string()})),
@@ -121,8 +117,8 @@ struct Post {
     post_content: String,
     post_category: String,
     post_tags: Vec<String>,
-    created_at: String,
-    updated_at: String,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
 }
 
 struct AppState {
@@ -131,21 +127,35 @@ struct AppState {
 
 impl AppState {
     async fn new(url: &'static str) -> Result<AppState, sqlx::Error> {
-        Ok(AppState {
+        let db = AppState {
             pool: Arc::new(PgPoolOptions::new().max_connections(5).connect(url).await?),
-        })
+        };
+
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS posts
+                (id SERIAL PRIMARY KEY,
+                post_title varchar(128) NOT NULL,
+                post_content TEXT NOT NULL,
+                post_category varchar(64) NOT NULL,
+                post_tags varchar(64)[] NOT NULL,
+                created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP)",
+        )
+        .execute(&mut *db.pool.acquire().await?)
+        .await?;
+
+        Ok(db)
     }
 
-    async fn create_post(&self, new_post: RawPost) -> Result<Post, sqlx::Error> {
+    async fn create_post(&self, new_post: RawPost) -> Result<i32, sqlx::Error> {
         let mut conn = self.pool.acquire().await?;
 
-        let post: Post= sqlx::query_as("INSERT INTO posts (post_title, post_content, post_category, post_tags, created_at, updated_at) values ($1, $2, $3, $4, $5, $6, $6)")
+        let post= sqlx::query("INSERT INTO posts (post_title, post_content, post_category, post_tags) values ($1, $2, $3, $4) RETURNING id")
             .bind(new_post.post_title)
             .bind(new_post.post_content)
             .bind(new_post.post_category)
             .bind(new_post.post_tags)
-            .bind(Utc::now())
-            .fetch_one(&mut *conn).await?;
+            .fetch_one(&mut *conn).await?.get("id");
 
         Ok(post)
     }
@@ -153,7 +163,7 @@ impl AppState {
     async fn delete_post(&self, post_id: i32) -> Result<(), sqlx::Error> {
         let mut conn = self.pool.acquire().await?;
 
-        sqlx::query("DELETE FROM posts WHERE post_id = $1")
+        sqlx::query("DELETE FROM posts WHERE id = $1")
             .bind(post_id)
             .execute(&mut *conn)
             .await?;
@@ -164,7 +174,7 @@ impl AppState {
     async fn read_post(&self, post_id: i32) -> Result<Post, sqlx::Error> {
         let mut conn = self.pool.acquire().await?;
 
-        let post = sqlx::query_as("SELECT * FROM posts WHERE post_id = $1")
+        let post: Post = sqlx::query_as("SELECT * FROM posts WHERE id = $1")
             .bind(post_id)
             .fetch_one(&mut *conn)
             .await?;
@@ -172,24 +182,24 @@ impl AppState {
         Ok(post)
     }
 
-    async fn update_post(&self, post_id: i32, updated_post: RawPost) -> Result<Post, sqlx::Error> {
+    async fn update_post(&self, post_id: i32, updated_post: RawPost) -> Result<(), sqlx::Error> {
         let mut conn = self.pool.acquire().await?;
 
-        sqlx::query("SELECT 1 FROM posts WHERE post_id = $1")
+        sqlx::query("SELECT 1 FROM posts WHERE id = $1")
             .bind(post_id)
-            .execute(&mut *conn)
+            .fetch_one(&mut *conn)
             .await?;
 
-        let post: Post = sqlx::query_as("UPDATE posts SET post_title = $1, post_content = $2, post_category = $3, post_tags = $4, updated_at = $5  WHERE post_id = $6")
+        sqlx::query("UPDATE posts SET post_title = $1, post_content = $2, post_category = $3, post_tags = $4, updated_at = $5  WHERE id = $6")
             .bind(updated_post.post_title)
             .bind(updated_post.post_content)
             .bind(updated_post.post_category)
             .bind(updated_post.post_tags)
             .bind(Utc::now())
             .bind(post_id)
-            .fetch_one(&mut *conn).await?;
+            .execute(&mut *conn).await?;
 
-        Ok(post)
+        Ok(())
     }
 
     async fn list_posts(&self) -> Result<Vec<Post>, sqlx::Error> {
@@ -204,7 +214,7 @@ impl AppState {
 }
 
 fn id_from_uri(uri: Uri) -> Result<i32, &'static str> {
-    match uri.path().rsplit('/').last().unwrap().parse::<i32>() {
+    match uri.path().split('/').last().unwrap().parse::<i32>() {
         Ok(id) => Ok(id),
         Err(_) => Err("invalid request ID"),
     }
